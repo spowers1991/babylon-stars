d7ce6144b88a84bb3892ce30702eb3464f3a97da
Steven Powers <stevenpowers1991@gmail.com>
2026-02-04 06:37:47 +0000

    optimisations added to ray picking

--- src/lib/Input/PointPicking/actions/handlePCSPicking.ts
+++ src/lib/Input/PointPicking/actions/handlePCSPicking.ts
@@ -2,5 +2,5 @@ import { pickParticlePCS } from "./pickParticlePCS";
 
 export function handlePCSPicking(controller: any, object: any) {
   const camera = controller.getCamera();
-  return pickParticlePCS(controller.scene, camera, object.pcs, 0.2);
+  return pickParticlePCS(controller.scene, camera, object.pcs);
 }
