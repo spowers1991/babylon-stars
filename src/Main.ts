dac350996071d4a1dfcadce75162cf8a0dec7d26
Steven Powers <stevenpowers1991@gmail.com>
2026-04-20 23:07:24 +0100

    added updates

--- src/Main.ts
+++ src/Main.ts
@@ -33,20 +33,20 @@ window.addEventListener("DOMContentLoaded", async () => {
 
   const milkyWay = galaxies.find(galaxy => galaxy.name === "Milky Way")!;
 
-
+/*
   const glow = new BABYLON.GlowLayer("glow", scene1 as any);
-  glow.intensity = 0.1;
-  
+  glow.intensity = 1;
+  */
  scene1.clearColor = new BABYLON.Color4(0, 0, 0, 1);
-
+/*
   setPostProcessing(scene1, {
     bloomEnabled: true,
     bloomThreshold: 0.25,
-    bloomWeight: 2,
+    bloomWeight: 0.2,
     bloomKernel: 164,
     bloomScale: 1.5,
   });
-
+*/
   setPointPicking(scene1, milkyWay);
 
   runRenders(engine, scene1);
